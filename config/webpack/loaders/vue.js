376469618b68b032cc7b17ab61372f204685c2a5
Alexandr Voinalovych <olexandr.voinalovych@gotoinc.co>
2022-02-06 12:22:14 +0200

    Fix styles for production

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -1,6 +1,14 @@
 module.exports = {
-  test: /\.vue(\.erb)?$/,
-  use: [{
-    loader: 'vue-loader'
-  }]
+	test: /\.vue(\.erb)?$/,
+	use: [
+		{
+			loader: 'vue-loader',
+			options: {
+				loaders: {
+					'scss': 'vue-style-loader!css-loader!sass-loader',
+					'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+				}
+			}
+		}
+	]
 }
